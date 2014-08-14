86cbe5205065b8f2ccd82e2a3b386a359a2ab5aa
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-08-14 12:25:47 -0400

    uninstall ref fix for 1.2.1

--- uninstall.js
+++ uninstall.js
@@ -2,20 +2,18 @@
 
 var path = require('path'),
 	fs = require('fs-extra'),
-	async = require('async'),
 	extensionFilePath = path.resolve(__dirname,'../../content/extensions/extensions.json'),
 	extensionFileJson = fs.readJSONSync(extensionFilePath),
 	packagejsonFileJSON = fs.readJSONSync(path.resolve('./package.json')),
 	extname = packagejsonFileJSON.name,
 	extpublicdir = path.resolve(__dirname,'../../public/extensions/', extname),
-	ExtensionCore = require('periodicjs.core.extensions'),
-	Extension = new ExtensionCore({
+	Extensions= require('periodicjs.core.extensions'),
+	ExtensionCore = new Extensions({
 		extensionFilePath: extensionFilePath 
 	});
 
 ExtensionCore.uninstall({
 		extname:extname,
-		extdir:extdir,
 		extpublicdir:extpublicdir,
 		extensionFileJson:extensionFileJson
 	},
